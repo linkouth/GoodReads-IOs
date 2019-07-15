d763105fea549bb66b720b461c6742d491c1b2a1
Timur <dkatimullin@gmail.com>
2019-07-15 16:40:09 +0400

    Initial Commit

---
+++ GoodReads/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodReads
+//
+//  Created by User on 15/07/2019.
+//  Copyright © 2019 Timur LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
