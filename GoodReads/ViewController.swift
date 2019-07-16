da539e047ca0da4510f97ac1aa0284e68522c1a3
Timur <dkatimullin@gmail.com>
2019-07-16 10:29:25 +0400

    Added home screen

--- GoodReads/ViewController.swift
+++ GoodReads/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
